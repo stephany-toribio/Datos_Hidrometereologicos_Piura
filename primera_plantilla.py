92dfda11898ba8368d3d624e0b448eeb0b8fc707
LeonardoCam <89529410+LeonardoCam@users.noreply.github.com>
2022-12-11 18:30:29 -0500

    Update primera_plantilla.py

--- primera_plantilla.py
+++ primera_plantilla.py
@@ -3,27 +3,9 @@ import pandas as pd
 import numpy as np 
 import urllib.request
 
-
-
-# id= 1wdbF1i-8jdePmWt1Hf7p6DuZJpaUE3Je
-@st.experimental_memo 
-def download_data():
-	#https://drive.google.com/uc?id=YOURFILEID
-	url =  "https://drive.google.com/uc?id=1wdbF1i-8jdePmWt1Hf7p6DuZJpaUE3Je"
-	output= 'data.xls'
-	gdown.download(url,output,quiet = False)
-	
-
-
-
-
-
 st.title('Datos Hidrometereológicos del Gobierno Regional Piura')
 
 #id = 1alnmXxvcOvu3o3UxL_41YwNmdLczgN1u
-'''
-
-'''
 
 @st.experimental_memo 
 def download_data():
@@ -36,8 +18,11 @@ def download_data():
 dt = download_data()
 
 st.markdown('''
-	El norte del país se encuentra instalado el Proyecto Especial Chira Piura, que se encarga de la administración de los recursos hídricos provenientes de los Valles del Chira y Piura, el fin de esto es el abastecimiento de agua para el sector agrícola que se desarrolla en la zona.
-	Ellos cuentan con 27 estaciones o puntos de toma del caudal de los ríos en cuestión y las precipitaciones que sobre estos influyen, por lo que el presente proyecto pretende sistematizar esta información para brindar gráficas que permitan comprender de manera más amigable el comportamiento de los ríos Chira y Piura, a fin de lograr una toma de decisiones más eficiente por parte de las personas implicadas en el sector agrícola.
+	El norte del país se encuentra instalado el Proyecto Especial Chira Piura, que se encarga de la administración de los recursos hídricos 
+	provenientes de los Valles del Chira y Piura, el fin de esto es el abastecimiento de agua para el sector agrícola que se desarrolla en la zona.
+	Ellos cuentan con 27 estaciones o puntos de toma del caudal de los ríos en cuestión y las precipitaciones que sobre estos influyen, 
+	por lo que el presente proyecto pretende sistematizar esta información para brindar gráficas que permitan comprender de manera más amigable 
+	el comportamiento de los ríos Chira y Piura, a fin de lograr una toma de decisiones más eficiente por parte de las personas implicadas en el sector agrícola.
 	Este dataset muestra los datos hidrometereológicos registrados de las presas, estaciones hidrológicas e hidrométricas.
 	- **Base de Datos:** (https://www.datosabiertos.gob.pe/node/10105/download)''')
 
@@ -49,43 +34,36 @@ dt = dt.drop(columns=['FECHA_CORTE', 'FECHA_MUESTRA', 'UNIDAD_MEDIDA', 'DEPARTAM
 st.dataframe(dt.describe())
 
 st.header('Estadistica Hidrometereológica del tipo de estacion')
+ 
 
-#selectbox de tipo de estaciones 
-lista_tipo_estacion = []
-for elem in dt['TIPO_ESTACION'].unique():
-  lista_tipo_estacion.append(elem)
-
-op1 = st.selectbox('- Seleccione el tipo de estacion', tuple(sorted(lista_tipo_estacion)))
+op1 = st.selectbox('- Seleccione el tipo de estacion', sorted(dt['TIPO_ESTACION'].unique()))
 st.write('Tipo de estacion seleccionada:', op1.capitalize())
 
-#grafico del tipo de estaciones
 df_tipo = dt[dt['TIPO_ESTACION'] == op1]
 
-graf1 = pd.DataFrame(
+st.line_chart(pd.DataFrame(
     df_tipo,
-    columns=['CAUDAL07H', 'PROMEDIO24H', 'MAXIMA24H', 'PRECIP24H'])
-st.line_chart(graf1)
+    columns=['CAUDAL07H', 'PROMEDIO24H', 'MAXIMA24H', 'PRECIP24H']))
 
-st.header('Gráficos por distrito según dato Hidrometereológico')
-
-#selectbox de provincias
-lista_provincia = []
-for elem in dt['PROVINCIA'].unique():
-  lista_provincia.append(elem)
-
-#op2 = st.selectbox('- Seleccione la provincia', tuple(sorted(lista_provincia)))
-
-#selectbox de distritos
-lista_distrito = []
-for elem in dt['DISTRITO'].unique():
-  lista_distrito.append(elem)
-
-op3 = st.selectbox('- Seleccione el distrito', tuple(sorted(lista_distrito)))
-st.write('Distrito seleccionado:', op3)
+st.header('Data Hidrometereológica del distrito seleccionado')
 
 
-#df_provincia = dt[dt['PROVINCIA'] == op2]
-df_distrito = dt[dt['DISTRITO'] == op3]
+op2 = st.selectbox('- Seleccione el distrito', sorted(dt['DISTRITO'].unique()))
+st.write('Distrito seleccionadO: ', op2)
+
+grupo = dt.groupby(dt.DISTRITO)
+distrito = grupo.get_group(op2)
+
+cont_distrito = distrito.iloc[:,5:]
+
+st.dataframe(cont_distrito)
+
+st.write(pd.DataFrame({'Max': distrito.iloc[:,5:].max(), 'Min': distrito.iloc[:,5:].min()}))
+
+st.subheader("Gráficos interactivos")
+st.bar_chart(cont_distrito.mean())
+
+df_distrito = dt[dt['DISTRITO'] == op2]
 
 datos_hidro = ['CAUDAL07H', 'PROMEDIO24H', 'MAXIMA24H', 'PRECIP24H']
 
@@ -93,17 +71,20 @@ for i in range(0, 4):
 	graf = pd.DataFrame(df_distrito , columns=[datos_hidro[i]])
 	st.caption('Grafico de ' + datos_hidro[i])
 	st.line_chart(graf)
-#
+
+st.header('Filtro de data Hidrometereológica por distrito')
 
 op_multi = st.multiselect(
-    "Seleccione las provincias que desea comparar", 
-    options= dt["PROVINCIA"].unique()
+    "Seleccione los distritos que desea comparar", 
+    options= dt["DISTRITO"].unique()
     )
 
-x = dt.set_index("PROVINCIA")
+op_dato = st.selectbox('- Seleccione el dato Hidrometereológico que desea comparar', sorted(datos_hidro))
+
+x = dt.set_index("DISTRITO")
 y = x.loc[op_multi]
 
 st.dataframe(y)
-z = x.loc[op_multi,"PROMEDIO24H"]
+z = x.loc[op_multi, op_dato]
 
 st.bar_chart(z)
