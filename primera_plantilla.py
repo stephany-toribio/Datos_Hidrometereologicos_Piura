5c163e3a940a38b80268fa1a1a1f1c38e6da9991
Victor Nikolai <118230173+Nikolai0Huarcaya@users.noreply.github.com>
2022-12-11 09:33:09 -0500

    Update primera_plantilla.py

--- primera_plantilla.py
+++ primera_plantilla.py
@@ -1,10 +1,8 @@
+
 import streamlit as st
 import pandas as pd
 import numpy as np 
 import urllib.request
-
-
-
 
 st.title('Datos Hidrometereológicos del Gobierno Regional Piura')
 
@@ -14,9 +12,6 @@ st.title('Datos Hidrometereológicos del Gobierno Regional Piura')
 def download_data():
 	url = 'https://www.datosabiertos.gob.pe/node/10105/download'
 	filename = 'datos_piura.csv'
-	#https://drive.google.com/uc?id=1alnmXxvcOvu3o3UxL_41YwNmdLczgN1u
-	#url = 'https://drive.google.com/uc?id=1alnmXxvcOvu3o3UxL_41YwNmdLczgN1u'
-	#gdown.download(url, filename, quiet=False)
 	urllib.request.urlretrieve(url, filename)
 	df = pd.read_csv(filename)
 	return df 
@@ -34,50 +29,54 @@ st.header('Resumen del dataset')
 dt = dt.drop(columns=['FECHA_CORTE', 'FECHA_MUESTRA', 'UNIDAD_MEDIDA', 'DEPARTAMENTO', 'UBIGEO'], axis=1)
 st.dataframe(dt.describe())
 
+st.header('Estadistica Hidrometereológica del tipo de estacion')
+
 #selectbox de tipo de estaciones 
 lista_tipo_estacion = []
 for elem in dt['TIPO_ESTACION'].unique():
   lista_tipo_estacion.append(elem)
 
-st.header('Seleccione los siguientes apartados para determinar el grafico')
-#mostrar las opciones
 op1 = st.selectbox('- Seleccione el tipo de estacion', tuple(sorted(lista_tipo_estacion)))
 st.write('Tipo de estacion seleccionada:', op1.capitalize())
 
-#selectbox de provincia
-lista_provincia = []
-for item in dt['PROVINCIA'].unique():
-	lista_provincia.append(item)
+#grafico del tipo de estaciones
+df_tipo = dt[dt['TIPO_ESTACION'] == op1]
 
-op2 = st.selectbox('- Seleccione la provincia', tuple(sorted(lista_provincia)))
-st.write('Provincia seleccionada:', op2)
-
-#selectbox de distrito
-lista_distrito = []	
-for comp in dt['DISTRITO'].unique():
-	lista_distrito.append(comp)
-
-op3 = st.selectbox('- Seleccion el distrito', tuple(sorted(lista_distrito)))
-st.write('Distrito seleccionado:', op3)
-
-#agrupar la respuesta de los selectbox
-df_total = dt[dt['TIPO_ESTACION'] == op1]
-df_provincia = df_total[df_total['PROVINCIA'] == op2]
-df_agrupado = df_total[df_total['DISTRITO'] == op3]
-
-#Graficos
-
-#grafico de lineas
-
-st.subheader('Grafico de Lineas')
 graf1 = pd.DataFrame(
-    df_agrupado,
+    df_tipo,
     columns=['CAUDAL07H', 'PROMEDIO24H', 'MAXIMA24H', 'PRECIP24H'])
 st.line_chart(graf1)
 
-#grafico de barras
-st.subheader('Grafico de barras')
-graf2 = pd.DataFrame(
-    df_agrupado,
-    columns=['CAUDAL07H', 'PROMEDIO24H', 'MAXIMA24H', 'PRECIP24H'])
-st.bar_chart(graf2)
+st.header('Grafico de provincia por dato Hidrometereológico')
+
+#selectbox de provincias
+lista_provincia = []
+for elem in dt['PROVINCIA'].unique():
+  lista_provincia.append(elem)
+
+op2 = st.selectbox('- Seleccione la provincia', tuple(sorted(lista_provincia)))
+
+#selectbox de distritos
+lista_distrito = []
+for elem in dt['DISTRITO'].unique():
+  lista_distrito.append(elem)
+
+op3 = st.selectbox('- Seleccione el distrito', tuple(sorted(lista_distrito)))
+st.write('Distrito seleccionada:', op3)
+
+
+df_provincia = dt[dt['PROVINCIA'] == op2]
+df_distrito = dt[dt['DISTRITO'] == op3]
+
+datos_hidro = ['CAUDAL07H', 'PROMEDIO24H', 'MAXIMA24H', 'PRECIP24H']
+columnas = [df_provincia, df_distrito]
+
+for j in range(0,2):
+	for i in range(0, 4):
+		graf = pd.DataFrame(columnas[j], columns=[datos_hidro[i]])
+		st.caption('Grafico de ' + datos_hidro[i])
+		st.line_chart(graf)
+
+
+
+
