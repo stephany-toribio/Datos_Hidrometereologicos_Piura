637ef90e91a96a354ddf9324db09517c0f915941
Victor Nikolai <118230173+Nikolai0Huarcaya@users.noreply.github.com>
2022-12-11 11:56:49 -0500

    Update primera_plantilla.py

--- primera_plantilla.py
+++ primera_plantilla.py
@@ -6,7 +6,7 @@ import plotly.express as px
 
 
 #Nikolai#
-excel_file = '11122022_bd_hdro_piura.csv'   #Nombre archivo a importar 
+excel_file = 'hidro_piura.xlsx'   #Nombre archivo a importar 
 sheet_name = '11122022_bd_hdro_piura'   #la hoja de excel que voy a importar
 
 df = pd.read_excel(excel_file, #importo el archivo excel
