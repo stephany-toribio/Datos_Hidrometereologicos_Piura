14eaf2c60aeb1dae3e30c8fc458ae0c5aaee0939
Victor Nikolai <118230173+Nikolai0Huarcaya@users.noreply.github.com>
2022-12-11 21:16:22 -0500

    Update primera_plantilla.py

--- primera_plantilla.py
+++ primera_plantilla.py
@@ -5,14 +5,7 @@ import urllib.request
 
 st.title('Datos Hidrometereológicos del Gobierno Regional Piura')
 
-
-from PIL import Image
-image = Image.open('contaminacion.jpeg')
-st.image(image, caption='Distribución de las estaciones en el Proyecto Especial Chira Piura', use_column_width=True)
-
-
 #id = 1alnmXxvcOvu3o3UxL_41YwNmdLczgN1u
-
 
 @st.experimental_memo 
 def download_data():
@@ -23,21 +16,33 @@ def download_data():
 	return df 
 
 dt = download_data()
+aux = dt
 
-st.markdown('''
-	El norte del país se encuentra instalado el Proyecto Especial Chira Piura, que se encarga de la administración de los recursos hídricos 
-	provenientes de los Valles del Chira y Piura, el fin de esto es el abastecimiento de agua para el sector agrícola que se desarrolla en la zona.
-	Ellos cuentan con 27 estaciones o puntos de toma del caudal de los ríos en cuestión y las precipitaciones que sobre estos influyen, 
-	por lo que el presente proyecto pretende sistematizar esta información para brindar gráficas que permitan comprender de manera más amigable 
-	el comportamiento de los ríos Chira y Piura, a fin de lograr una toma de decisiones más eficiente por parte de las personas implicadas en el sector agrícola.
-	Este dataset muestra los datos hidrometereológicos registrados de las presas, estaciones hidrológicas e hidrométricas.
-	- **Base de Datos:** (https://www.datosabiertos.gob.pe/node/10105/download)''')
+# lista_x = []
+# a = list(dt['FECHA_MUESTRA'])
+# for i in a:
+#   lista_x.append((str(i))[:4] + '-' + (str(i))[4:6] + '-' + (str(i))[6:])
 
 
-########################
+##con el texto justificado :3
+st.markdown("""<div style="text-align: justify;">
+	Este dataset muestra los datos hidrometereológicos registrados de las presas, 
+	estaciones hidrológicas e hidrométricas de la región de Piura. 
+	La importancia de conocer datos hidrometereológicos se refleja en la prevención
+	de desastres naturales.
+	\n\nTener en cuenta que, de la base de datos que rescatamos, no tenemos información de
+	la capacidad máxima del caudal que tiene como máximo una determinada cuenca.
+	Los datos se muestra está para el uso a disposición para analizar de manera 
+	independiente cada una.
+	\n\n- **Base de Datos:** _última actualización: 2022-11-06_
+	</div>
+	https://www.datosabiertos.gob.pe/node/10105/
+	
+	""",unsafe_allow_html=True)
 
-#st.header('Dataset Hidrometereológico')  #MODIFICACIÓN
-#st.dataframe(dt) #MODIFICACIÓN 
+
+st.header('Dataset Hidrometereológico')  MODIFICACIÓN
+st.dataframe(dt) MODIFICACIÓN 
 
 st.header('Resumen del dataset')
 dt = dt.drop( ##eliminamos columnas de las que no nos pueden beneficiar 
@@ -61,69 +66,136 @@ st.markdown("""<div style="text-align: justify;">
 	</div>
 	""",unsafe_allow_html=True)
 
-####################
 
-st.header('Dataset Hidrometereológico')
-st.dataframe(dt)
 
-st.header('Resumen del dataset')
-dt = dt.drop(columns=['UNIDAD_MEDIDA', 'DEPARTAMENTO', 'UBIGEO'], axis=1)
-st.dataframe(dt.describe())
+st.header('Estadistica Hidrometereológica del tipo de estación')
 
-st.header('Estadistica Hidrometereológica del tipo de estacion')
- 
+#selectbox de tipo de estaciones 
+lista_tipo_estacion = []
+for elem in dt['TIPO_ESTACION'].unique():
+  lista_tipo_estacion.append(elem)
 
-op1 = st.selectbox('- Seleccione el tipo de estacion', sorted(dt['TIPO_ESTACION'].unique()))
+op1 = st.selectbox('- Seleccione el tipo de estación', tuple(sorted(lista_tipo_estacion)))
 st.write('Tipo de estacion seleccionada:', op1.capitalize())
 
+#grafico del tipo de estaciones
 df_tipo = dt[dt['TIPO_ESTACION'] == op1]
 
-st.line_chart(pd.DataFrame(
+graf1 = pd.DataFrame(
     df_tipo,
-    columns=['CAUDAL07H', 'PROMEDIO24H', 'MAXIMA24H', 'PRECIP24H']))
+    columns=['CAUDAL07H', 'PROMEDIO24H', 'MAXIMA24H', 'PRECIP24H'])
+st.line_chart(graf1)
 
-st.header('Data Hidrometereológica del distrito seleccionado')
+st.header('Grafico de provincia por dato Hidrometereológico')
+
+#selectbox de provincias
+lista_provincia = []
+for elem in dt['PROVINCIA'].unique():
+  lista_provincia.append(elem)
+
+op2 = st.selectbox('- Seleccione la provincia', tuple(sorted(lista_provincia)))
+
+#selectbox de distritos
+lista_distrito = []
+for elem in dt['DISTRITO'].unique():
+  lista_distrito.append(elem)
+
+op3 = st.selectbox('- Seleccione el distrito', tuple(sorted(lista_distrito)))
+st.write('Distrito seleccionada:', op3)
 
 
-op2 = st.selectbox('- Seleccione el distrito', sorted(dt['DISTRITO'].unique()))
-st.write('Distrito seleccionado: ', op2)
-
-grupo = dt.groupby(dt.DISTRITO)
-distrito = grupo.get_group(op2)
-
-cont_distrito = distrito.iloc[:,5:]
-
-st.dataframe(cont_distrito)
-
-st.write(pd.DataFrame({'Max': distrito.iloc[:,5:].max(), 'Min': distrito.iloc[:,5:].min()}))
-
-st.subheader("- Gráficos interactivos")
-st.bar_chart(cont_distrito.mean())
-
-df_distrito = dt[dt['DISTRITO'] == op2]
+df_provincia = dt[dt['PROVINCIA'] == op2]
+df_distrito = dt[dt['DISTRITO'] == op3]
 
 datos_hidro = ['CAUDAL07H', 'PROMEDIO24H', 'MAXIMA24H', 'PRECIP24H']
+## separamos las fechas por un 
+
 
 for i in range(0, 4):
-	graf = pd.DataFrame(df_distrito , columns=[datos_hidro[i]])
-	st.caption('Grafico de ' + datos_hidro[i])
-	st.line_chart(graf)
+	graf = pd.DataFrame(df_provincia , columns=[datos_hidro[i]])
+	st.subheader('Grafico de ' + datos_hidro[i])#(st.caption('Grafico de ' + datos_hidro[i]))
+	st.line_chart(graf)#################################################################
+	st.caption('x = índice de la fila del dataframe o dataset\n		y = valor obtenido en la observación')
+#
+grupo = dt.groupby(dt.PROVINCIA)
+provincia = grupo.get_group(op2)
 
-st.header('Filtro de data Hidrometereológica por distrito')
+
+cont_distrito = provincia.iloc[:,5:].max()
+
+
+
+st.subheader("Datos hidrometereológicos del distrito seleccionado - Resume") 
+#####################
+column_ , column__ = st.columns(2)
+with column_:
+	
+    st.write('valores máximos y mínimos captados')
+# with column__:
+	
+#     st.write('valores mínimos captados')
+#	se cambió pq te fusionó ambas tablitas de max y min
+###################
+
+
+# with column1:
+	
+#     st.dataframe(cont_distrito)
+# with column2:
+	
+#     st.dataframe(provincia.iloc[:,5:].min())
+
+
+aus = pd.DataFrame({'Max': provincia.iloc[:,5:].max(), 'Min': provincia.iloc[:,5:].min()})
+st.write(aus)
+
+# st.write('los datos mecionados del maximo corresponden a: \nMáximo\n', dt.loc[dt ['CAUDAL07H']  == aus["CAUDAL07H"]["max"]],
+# dt.loc[dt ['']  == aus["CAUDAL07H"]["max"]]
+# )
+st.markdown("""
+En los estadísticos siguientes, se muestra según la selección por el usuario (en la lista de opciones anterior),
+\n_count_: es un total o sumatoria de las observaciones de la cuenca anteriormente seleccionada
+\n_mean_: media o promedio de los datos obtenidos 
+\n_std_: variabilidad de mis observaciones respecto a la media de los datos obtenidos en la cuenca
+\n_min_: el valor mínimo de todas las observaciones de la cuenca según mis variables que se muestran en el encabezado
+\n_25%_ De la ocurrencia de mis datos es menor al valor mostrado
+\n_50%_ De la ocurrencia de mis datos es menor al valor mostrado
+\n_75%_: De la ocurrencia de mis datos es menor al valor mostrado
+\n\tlos porcentajes son mis percentiles o cuartiles 1,2,3 respectivamente.
+\n_max_: el valor máximo de todas las observaciones de la cuenca según las variables que se muestran en el encabezado
+
+"""
+)
+
+st.write(provincia.iloc[:,5:].describe())
+
+st.subheader("Gráficos interactivos")
+st.bar_chart(cont_distrito.describe())
+#S
 
 op_multi = st.multiselect(
-    "- Distritos", 
-    options= dt["DISTRITO"].unique()
+    "Seleccione las provincias que desea comparar", 
+    options= dt["PROVINCIA"].unique()
     )
 
-op_dato = st.selectbox('- Dato Hidrometereológico', sorted(datos_hidro))
-
-x = dt.set_index("DISTRITO")
+x = dt.set_index("PROVINCIA")
 y = x.loc[op_multi]
 
 st.dataframe(y)
-z = x.loc[op_multi, op_dato]
+z = x.loc[op_multi,"PROMEDIO24H"]
 
 st.bar_chart(z)
+
+st.write("""Caudal: es el volumen de agua por unidad de tiempo que pasa por una sección de un
+cauce. _Las unidades en las que se mide es en """)
+st.latex("m/s^3")
+st.write(
+"""\n
+- Las observaciones han sido obtenidas en diferentes fechas
+- El caudal ha sido tomado cada 7 horas
+- El promedio es referente al cauda durante 24 horas
+- Maximo valor del caudal en el periodo de 24 horas
+- Precipitación en 24 horas
+""")
 
 
