4ca70a41271d4970071fce128e39986006902d4c
Victor Nikolai <118230173+Nikolai0Huarcaya@users.noreply.github.com>
2022-12-11 21:32:07 -0500

    Update primera_plantilla.py

--- primera_plantilla.py
+++ primera_plantilla.py
@@ -101,6 +101,21 @@ st.bar_chart(z)
 
 
 
+st.write("""Caudal: es el volumen de agua por unidad de tiempo que pasa por una sección de un
+cauce. _Las unidades en las que se mide es en """)
+st.latex("m/s^3")
+st.write(
+"""\n
+- Las observaciones han sido obtenidas en diferentes fechas
+- El caudal ha sido tomado cada 7 horas
+- El promedio es referente al cauda durante 24 horas
+- Maximo valor del caudal en el periodo de 24 horas
+- Precipitación en 24 horas
+""")
+
+
+
+
 @st.cache
 def convert_df(df):
     return df.to_csv().encode('utf-8')
